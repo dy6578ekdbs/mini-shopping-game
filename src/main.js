9af08a529d0959718d1baf147ab5e18091262c1a
dayun <dy6578ekdbs@gmail.com>
2021-10-18 23:41:32 +0900

    handling events

--- src/main.js
+++ src/main.js
@@ -19,11 +19,34 @@ function createHTMLString(item){
     `;
 }
 
+function onButtonClick(event,items){
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null){
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+
+    displayItems(filtered);
+   
+  
+}
+
+function setEventListeners(items){
+    const logo = document.querySelector('.logo');
+    const buttons  = document.querySelector('.buttons'); //버튼 여러개 한번에???
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event,items));
+}
+
 //함수 시간이 걸리니까, 성공적으로 되면, 그때 실행! -> promise
 //main함수!
 loadItems()
 .then(items => {
     displayItems(items); //보여주고
-   // setEventListeners(items); //이벤트까지
+   setEventListeners(items); //이벤트까지
 })
 .catch(console.log);
